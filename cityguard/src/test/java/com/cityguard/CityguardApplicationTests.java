e22b1e25fd6c50341774c57d6707971635b584b8
mayuresh-kakade <msk.kakade@gmail.com>
2026-09-20 22:10:04 +0530

    Configure CityGuard Spring Boot And MYSQL

---
+++ cityguard/src/test/java/com/cityguard/CityguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cityguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
